c65322ac9449df0d8f569fc81ec3c5b0af2ec876
Vitinho <vitinho.dev@gmail.com>
2021-03-18 14:18:27 -0300

    Lendo os dados do arquivo upado

--- src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
@@ -5,7 +5,7 @@ import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
   handle(req: Request, res: Response): Response {
-    const { file } = req.body;
+    const { file } = req;
 
     this.importCategoryUseCase.execute(file);
 
