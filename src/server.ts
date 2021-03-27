66a66ff5cb44b80af13cc06e3d65547187d3ee18
Vitinho <vitinho.dev@gmail.com>
2021-03-27 19:45:47 -0300

    Adição de um redirecionador de erros

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import swaggerUi from "swagger-ui-express";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
 import "./database";
-
 import "./shared/container";
 
 const app = express();
@@ -16,5 +16,18 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal Server Error - ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
