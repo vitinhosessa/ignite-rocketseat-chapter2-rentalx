9a9bf992684b4dc1ffe73797ff689abb896c84f3
Vitinho <vitinho.dev@gmail.com>
2021-03-29 13:04:38 -0300

    Lint correction

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -8,6 +8,7 @@ interface IPayload {
   sub: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function ensureAuthenticated(
   req: Request,
   res: Response,
