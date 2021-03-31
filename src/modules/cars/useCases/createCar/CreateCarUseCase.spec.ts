a364e7f2a93aa231b6eff730b836afc808ca6308
Vitinho <vitinho.dev@gmail.com>
2021-03-31 16:47:38 -0300

    Nova rota cars para criação (TDD). Criado e Implementado

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepository);
+  });
+
+  it("should be able to create a new car", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "DEV-1337",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new car with exists license plate", async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Name Car",
+        description: "Description Car",
+        daily_rate: 100,
+        license_plate: "DEV-1337",
+        fine_amount: 60,
+        brand: "Brand",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Name Car 2",
+        description: "Description Car 2",
+        daily_rate: 100,
+        license_plate: "DEV-1337",
+        fine_amount: 60,
+        brand: "Brand",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a new car with available true by deffault", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Car Available",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC-1337",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+
+    expect(car.available).toBe(true);
+  });
+});
