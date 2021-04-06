c8e7b20fb372500366edb05199eef3b941d9f92d
Vitinho <vitinho.dev@gmail.com>
2021-04-06 16:19:16 -0300

    fix: novas variaveis para conseguir dar update

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -7,9 +7,12 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
 
   async create({
-    car_id,
     user_id,
+    car_id,
     expected_return_date,
+    id,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = new Rental();
 
@@ -17,6 +20,9 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
       car_id,
       user_id,
       expected_return_date,
+      id,
+      end_date,
+      total,
       start_date: new Date(),
     });
 
