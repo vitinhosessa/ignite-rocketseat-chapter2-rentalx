c167e9bc756d16b7b28511472377f71ce9726daf
Vitinho <vitinho.dev@gmail.com>
2021-04-06 17:04:24 -0300

    feat: consultar aluguel pelo id do usuario

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,7 +5,8 @@ interface IRentalsRepository {
   create(data: ICreateRentalDTO): Promise<Rental>;
   findOpenRentalByCarID(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
-  findByID(id: string): Promise<Rental>;
+  findByID(rental_id: string): Promise<Rental>;
+  findByUserID(user_id: string): Promise<Rental[]>;
 }
 
 export { IRentalsRepository };
