8eacd9cb8122ce26ca986a8b38905fe045b18e16
Vitinho <vitinho.dev@gmail.com>
2021-04-07 16:32:15 -0300

    feat: criado repositorio typeorm e InMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,28 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  usersTokens: UserTokens[] = [];
+
+  async create({
+    user_id,
+    expires_date,
+    refresh_token,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = new UserTokens();
+
+    Object.assign(userToken, {
+      user_id,
+      expires_date,
+      refresh_token,
+    });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
