3ab0321d95d7df8ee45651ca9ae53e34b2a71f8f
Vitinho <vitinho.dev@gmail.com>
2021-04-07 16:34:32 -0300

    feat: criado um arquivo de config com dados token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secretToken: "9516a0c2c7af36f59333d00364e24dbd",
+  expiresInToken: "15m",
+  secretRefreshToken: "943aa6f01fa7ba424910cb1ff028fc8d",
+  expiresInRefreshToken: "30d",
+  expiresRefreshTokenDays: 30,
+};
