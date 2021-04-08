f6f016c9397c7bb1ba5510326667bfddd472c0c5
Vitinho <vitinho.dev@gmail.com>
2021-04-08 18:26:12 -0300

    feat: nova rota reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
+import { ResetPasswordController } from "@modules/accounts/UseCases/resetPassword/ResetPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/UseCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
