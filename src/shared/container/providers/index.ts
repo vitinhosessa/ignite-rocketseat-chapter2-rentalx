9e4a798e123a4f7fe3959bd1a4175e6efa772c26
Vitinho <vitinho.dev@gmail.com>
2021-04-08 17:50:43 -0300

    feat: declaração do provider de email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,17 @@
 import { container } from "tsyringe";
 
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+
 import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherealMailProvider",
+  new EtherealMailProvider()
 );
