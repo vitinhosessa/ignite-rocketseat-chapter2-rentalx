d8861c88962917f904d03fb6a79380ee763d28de
Vitinho <vitinho.dev@gmail.com>
2021-04-12 15:56:36 -0300

    refactor: nome de pasta estava com padrão errado

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { ResetPasswordController } from "@modules/accounts/UseCases/resetPassword/ResetPasswordController";
-import { SendForgotPasswordMailController } from "@modules/accounts/UseCases/sendForgotPasswordMail/SendForgotPasswordMailController";
+import { ResetPasswordController } from "@modules/accounts/useCases/resetPassword/ResetPasswordController";
+import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
