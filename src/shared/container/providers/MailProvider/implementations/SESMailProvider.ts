62497f2fd71bcbadddd999cc5b0f0a4bb10e257f
Vitinho <vitinho.dev@gmail.com>
2021-04-14 17:24:36 -0300

    feat: implementado AWS SES como mailProvider

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,42 @@
+import aws from "aws-sdk";
+import fs from "fs";
+import handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../IMailProvider";
+
+injectable();
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+  constructor() {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: "2010-12-01",
+        region: process.env.AWS_SES_REGION,
+      }),
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      from: "RentalX <noreplay@rentalx.com>", // Email cadastrado na AWS
+      subject,
+      html: templateHTML,
+    });
+  }
+}
+
+export { SESMailProvider };
